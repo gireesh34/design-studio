a7a9109bdd134ccf5a536bde1cd15f395635295b
gireesh34 <152199178+gireesh34@users.noreply.github.com>
2024-11-30 20:15:56 +0530

    theme updated

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,5 @@
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
-import aspectRatio from '@tailwindcss/aspect-ratio';
-
 export default {
   plugins: {
-    forms,
-    typography,
-    aspectRatio,
     tailwindcss: {},
     autoprefixer: {},
   },
